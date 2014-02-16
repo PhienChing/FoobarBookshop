3a0e477ad65122b03e7296890af7d39ef61d7557
kimtillas91 <kimberly.cantillas@gmail.com>
2014-02-17 06:42:54 +0800

    Create login.php
    template

---
+++ application/controllers/login.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+  private function index() {
+    /* check login credentials against database */
+    
+    /* check position */
+    
+    /* load page for apt position */
+  }
+
+}
