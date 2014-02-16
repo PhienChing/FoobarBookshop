870f8f0e681ec60e1e7c696ed7b24f81af579b23
kimtillas91 <kimberly.cantillas@gmail.com>
2014-02-17 06:39:45 +0800

    Update db_viewer.php
    minor edit

--- application/models/db_viewer.php
+++ application/models/db_viewer.php
@@ -1,5 +1,8 @@
-<?php
-/*insert security lalala*/
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/* This class is only for viewing the database.
+ * Updates, inserts, deletes must be made using the Db_updater class
+ */
 
 class Db_viewer extends CI_Model {
 
