99b822aa9f6d631f5f9a4951a2a7f51d72591b6c
kimtillas91 <kimberly.cantillas@gmail.com>
2014-02-17 06:38:25 +0800

    Create db_updater.php
    empty starting template

---
+++ application/models/db_updater.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/* This class is only for updating the database.
+ * Select statements must be made using the Db_viewer class
+ */
+
+class Db_updater extends CI_Model {
+
+}
