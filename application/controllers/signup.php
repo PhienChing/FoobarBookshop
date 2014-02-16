bfcdfce8267e060ff0af11bd9a5fc7210c28b96b
kimtillas91 <kimberly.cantillas@gmail.com>
2014-02-17 07:10:33 +0800

    Create signup.php
    template

---
+++ application/controllers/signup.php
@@ -0,0 +1,22 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Signup extends CI_Controller {
+
+  public function index() {
+    /* verify if unique account */
+    
+    
+  }
+  
+  function verifyUniqueness() {
+    /* check if username already exists */
+    
+    /* check if email already exists */
+    
+  }
+  
+  function createAccount() {
+  
+  }
+
+}
